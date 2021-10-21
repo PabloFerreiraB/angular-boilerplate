83b236658c6efcc55d4da3c8c516e7e1af8dbb05
Pablo Ferreira <pablo.branzan@capitanigroup.com.br>
2021-10-21 01:17:28 -0300

    feat: add NGXS state management

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,13 +1,20 @@
 import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
 import { CoreRoutes } from '@core/core.routes';
+import { NgxsReduxDevtoolsPluginModule } from '@ngxs/devtools-plugin';
+import { NgxsModule } from '@ngxs/store';
 import { CoreModule } from './@core/core.module';
 import { AppComponent } from './app.component';
 
-
 @NgModule({
   declarations: [AppComponent],
-  imports: [BrowserModule, CoreModule, CoreRoutes],
+  imports: [
+    BrowserModule,
+    CoreModule,
+    CoreRoutes,
+    NgxsModule.forRoot([]),
+    NgxsReduxDevtoolsPluginModule.forRoot(),
+  ],
   bootstrap: [AppComponent],
 })
 export class AppModule {}
